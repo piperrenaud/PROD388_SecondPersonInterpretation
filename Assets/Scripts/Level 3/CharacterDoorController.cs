28e0255a800e2fc64b147dad45cf2c699ddfef72
piperrenaud <piper@ignavus.net>
2026-09-10 18:52:51 +1200

    Idek

--- Assets/Scripts/Level 3/CharacterDoorController.cs
+++ Assets/Scripts/Level 3/CharacterDoorController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -22,10 +23,16 @@ public class CharacterDoorController : MonoBehaviour
 
     private MehcnaicalDoors targetDoor;
     private Room targetRoom;
-    private AudioSource source;
+
     private Vector3 targetPosition;
 
+    private KeyObject currentObjectiveTarget;
+    private LockedCabinet currentCabinetTarget;
+
     private float footstepTimer;
+    private AudioSource source;
+
+    public Room CurrentRoom => currentRoom;
 
     private void Start()
     {
@@ -47,6 +54,8 @@ public class CharacterDoorController : MonoBehaviour
 
     public void CheckOpenDoor()
     {
+        if (DoorManager.Instance == null) return;
+
         MehcnaicalDoors openDoor = DoorManager.Instance.CurrentOpenDoor;
 
         //no doors open
@@ -94,6 +103,21 @@ public class CharacterDoorController : MonoBehaviour
 
     private void Update()
     {
+        //going to a KeyObject
+        if (currentObjectiveTarget != null)
+        {
+            UpdateObjectiveTarget();
+            return;
+        }
+
+        //going to a locked cabinet
+        if (currentCabinetTarget != null)
+        {
+            UpdateCabinetTarget();
+            return;
+        }
+        
+        //not travelling through a door
         if (targetDoor == null)
         {
             animator.SetBool("IsRunning", false);
@@ -107,35 +131,14 @@ public class CharacterDoorController : MonoBehaviour
             targetDoor = null;
             targetRoom = null;
 
-
-
             StopMoving();
             return;
         }
 
         if (agent.pathPending) return;
 
-        bool isMoving = agent.velocity.sqrMagnitude > 0.01f;
+        UpdateMovementAnimation();
 
-        animator.SetBool("IsRunning", isMoving);
-
-        //footsteps
-        if (isMoving)
-        {
-            footstepTimer -= Time.deltaTime;
-
-            if (footstepTimer <= 0f)
-            {
-                PlayFootstep();
-                footstepTimer = footstepInterval;
-            }
-        }
-        else
-        {
-            footstepTimer = 0f;
-        }
-
-        //reached target point
         float distanceToTarget = Vector3.Distance(
             transform.position, targetPosition);
 
@@ -152,9 +155,36 @@ public class CharacterDoorController : MonoBehaviour
         targetDoor = null; 
         targetRoom = null; 
 
-        StopMoving(); 
+        StopMoving();
 
-        Debug.Log("Character entered: " + currentRoom.roomName);
+        //check if current objective has target in room
+        CheckForObjectiveTarget();
+    }
+
+    private void CheckForObjectiveTarget()
+    {
+        if (ObjectiveManager.Instance == null) return;
+        if (!ObjectiveManager.Instance.ObjectiveActive) return;
+
+        //check locked cabinet
+        LockedCabinet cabinet = GetObjectiveCabinet(currentRoom);
+
+        if (cabinet != null)
+        {
+            GoToCabinet(cabinet);
+            return;
+        }
+        
+        // check KeyObjects
+        KeyObject keyObject = GetObjectiveTarget(currentRoom);
+
+        if (keyObject != null)
+        {
+            GoToObjectiveTarget(keyObject);
+            return;
+        }
+
+        
     }
 
     private void StopMoving()
@@ -178,5 +208,163 @@ public class CharacterDoorController : MonoBehaviour
         source.pitch = Random.Range(minPitch, maxPitch);
 
         source.PlayOneShot(clip, volume);
+    }
+
+    private KeyObject GetObjectiveTarget(Room room)
+    {
+        KeyObject[] objects = FindObjectsByType<KeyObject>(FindObjectsSortMode.None);
+
+        foreach (KeyObject keyObject in objects)
+        {
+            if (keyObject.Room != room) continue;
+            if (!keyObject.IsCurrentObjective()) continue;
+
+            return keyObject;
+        }
+
+        return null;
+    }
+
+    private LockedCabinet GetObjectiveCabinet(Room room)
+    {
+        if (ObjectiveManager.Instance == null) return null;
+        if (!ObjectiveManager.Instance.ObjectiveActive) return null;
+
+        int currentObjective = ObjectiveManager.Instance.CurrentObjectiveIndex;
+
+        LockedCabinet[] cabinets = FindObjectsByType<LockedCabinet>(FindObjectsSortMode.None);
+
+        foreach (LockedCabinet cabinet in cabinets)
+        {
+            if (cabinet.Room != room) continue;
+
+            if (cabinet.ObjectiveOrder != currentObjective) continue;
+
+            if (!cabinet.IsCurrentObjective()) continue;
+
+            return cabinet;
+        }
+        
+        return null;
+    }
+
+    private void GoToObjectiveTarget(KeyObject objectiveTarget)
+    {
+        if (objectiveTarget.TargetPoint == null) return;
+
+        currentObjectiveTarget = objectiveTarget;
+
+        targetPosition = objectiveTarget.TargetPoint.position;
+
+        agent.isStopped = false;
+        agent.SetDestination(targetPosition);
+
+        animator.SetBool("IsRunning", true);
+    }
+
+    private void UpdateObjectiveTarget()
+    {
+        if (currentObjectiveTarget == null) return;
+
+        //objective changed while travellign
+        if (!currentObjectiveTarget.IsCurrentObjective())
+        {
+            currentObjectiveTarget = null;
+            StopMoving();
+            return;
+        }
+
+        if (agent.pathPending) return;
+
+        UpdateMovementAnimation();
+
+        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+
+        if (distanceToTarget <= roomDetectionDistance)
+        {
+            ReachObjectiveTarget();
+        }
+    }
+
+    private void ReachObjectiveTarget()
+    {
+        if (currentObjectiveTarget == null) return;
+
+        StopMoving();
+
+        KeyObject target = currentObjectiveTarget;
+
+        currentObjectiveTarget = null;
+
+        target.ReachTarget();
+
+        //objective mightve changed, check if new objective has target in current room
+        CheckForObjectiveTarget();
+    }
+
+    private void GoToCabinet(LockedCabinet cabinet)
+    {
+        if (cabinet == null) return;
+
+        if (cabinet.TargetPoint == null) return;
+
+        currentCabinetTarget = cabinet;
+        targetPosition = cabinet.TargetPoint.position;
+
+        agent.isStopped = false;
+        agent.SetDestination(targetPosition);
+
+        animator.SetBool("IsRunning", true);
+    }
+
+    private void UpdateCabinetTarget()
+    {
+        if (currentCabinetTarget == null) return;
+        if (agent.pathPending) return;
+
+        UpdateMovementAnimation();
+
+        float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
+
+        if (distanceToTarget <= roomDetectionDistance)
+        {
+            ReachCabinet();
+        }
+    }
+
+    private void ReachCabinet()
+    {
+        if (currentCabinetTarget == null) return;
+
+        StopMoving();
+
+        LockedCabinet cabinet = currentCabinetTarget;
+
+        currentCabinetTarget = null;
+
+        //actually interact with cabinet
+        cabinet.TryOpen(GetComponent<CharacterInventory>(), currentRoom);
+    }
+
+    private void UpdateMovementAnimation()
+    {
+        bool isMoving = agent.velocity.sqrMagnitude > 0.01f;
+
+        animator.SetBool("IsRunning", isMoving);
+
+        if (isMoving)
+        {
+            footstepTimer -= Time.deltaTime;
+
+            if (footstepTimer <= 0f)
+            {
+                PlayFootstep();
+                footstepTimer = footstepInterval;
+            }
+        }
+        else
+        {
+            footstepTimer = 0f;
+        }
     }
 }
